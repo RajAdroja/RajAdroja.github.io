3f884c92bf9b9f1832e40354c1b1911e784d8f99
RajAdroja <rajdineshbhai.adroja@sjsu.edu>
2024-12-22 17:06:49 -0800

    Updating the project with new project images

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,13 +1,17 @@
 import styles from './ProjectStyles.module.css'
-import viberr from '../../assets/viberr.png'
+import viberr from '../../assets/viberr.webp'
 import ProjectCard from '../../common/projectcard'
-import freshBurger from '../../assets/fresh-burger.png'
-import fitLift from '../../assets/fitlift.png'
-import projectphoto from '../../assets/projectphoto.png'
-import getsettravellogo from '../../assets/getsettravellogo.png'
+import fitLift from '../../assets/fitlift.webp'
+import projectphoto from '../../assets/projectphoto.webp'
+import getsettravellogo from '../../assets/getsettravellogo.webp'
 import golangicon from '../../assets/golangicon.png'
 import dockerlogo from '../../assets/dockerlogo.png'
 import crudlogo from '../../assets/CRUDlogo.png'
+import restaurantfinderlogo from '../../assets/restaurantfinderlogo.webp'
+import logparserlogo from '../../assets/logparserlogo.webp'
+import personalizedreciperecommenderlogo from '../../assets/personalizedreciperecommenderlogo.webp'
+import db2apilogo from '../../assets/db2apilogo.webp'
+import verdictailogo from '../../assets/verdictailogo.webp'
 
 function Projects() {
   return (
@@ -18,6 +22,11 @@ function Projects() {
           <ProjectCard src={projectphoto} link={"https://github.com/RajAdroja/Sentiment-Analysis"} h3={"ML Project"} p={"Sentiment Analysis"}/>
           <ProjectCard src={fitLift} link={"https://www.vpropel.in/"} h3={"V-PROPEL portal"} p={"Coding Platform"}/>
           <ProjectCard src={getsettravellogo} link={"https://github.com/RajAdroja/GetSetTravel"} h3={"GetSetTravel"} p={"Frontend website"}/>
+          <ProjectCard src={restaurantfinderlogo} link={"https://github.com/RajAdroja/RestaurantFinder"} h3={"RestaurantFinder"} p={"End2End WebApp"}/>
+          <ProjectCard src={logparserlogo} link={"https://github.com/RajAdroja/LogParser"} h3={"LogParser"} p={"Java Project"}/>
+          <ProjectCard src={personalizedreciperecommenderlogo} link={"https://github.com/RajAdroja/Personalized-Recipe-Recommender"} h3={"Recipe Recommender"} p={"ML Project"}/>
+          <ProjectCard src={db2apilogo} link={"https://github.com/RajAdroja/db2api"} h3={"db2appi"} p={"Schema Translation"}/>
+          <ProjectCard src={verdictailogo} link={"https://github.com/RajAdroja/db2api"} h3={"VerdictAI"} p={"LLM Project"}/>
         </div>
         <br />
         <br />
