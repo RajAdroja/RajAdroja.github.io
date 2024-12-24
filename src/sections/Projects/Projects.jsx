83186a6aad0198ff3201c0bf1c65121ba933648f
RajAdroja <rajdineshbhai.adroja@sjsu.edu>
2024-12-23 23:49:12 -0800

    adding customer churn prediction project

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -12,6 +12,7 @@ import logparserlogo from '../../assets/logparserlogo.webp'
 import personalizedreciperecommenderlogo from '../../assets/personalizedreciperecommenderlogo.webp'
 import db2apilogo from '../../assets/db2apilogo.webp'
 import verdictailogo from '../../assets/verdictailogo.webp'
+import customerchurnpredictionlogo from '../../assets/customerchurnpredictionlogo.webp'
 
 function Projects() {
   return (
@@ -26,7 +27,8 @@ function Projects() {
           <ProjectCard src={logparserlogo} link={"https://github.com/RajAdroja/LogParser"} h3={"LogParser"} p={"Java Project"}/>
           <ProjectCard src={personalizedreciperecommenderlogo} link={"https://github.com/RajAdroja/Personalized-Recipe-Recommender"} h3={"Recipe Recommender"} p={"ML Project"}/>
           <ProjectCard src={db2apilogo} link={"https://github.com/RajAdroja/db2api"} h3={"db2appi"} p={"Schema Translation"}/>
-          <ProjectCard src={verdictailogo} link={"https://github.com/RajAdroja/db2api"} h3={"VerdictAI"} p={"LLM Project"}/>
+          <ProjectCard src={verdictailogo} link={"https://github.com/RajAdroja/VerdictAI"} h3={"VerdictAI"} p={"LLM Project"}/>
+          <ProjectCard src={customerchurnpredictionlogo} link={"https://github.com/RajAdroja/Customer-Churn-Prediction"} h3={"Customer Churn"} p={"End to End ML pipeline"}/>
         </div>
         <br />
         <br />
