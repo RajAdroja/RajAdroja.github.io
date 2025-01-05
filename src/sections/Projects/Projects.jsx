f71f1d06701e559bcf87eca51570770deabbd358
RajAdroja <rajdineshbhai.adroja@sjsu.edu>
2025-01-04 22:44:59 -0800

    updating project section

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -13,12 +13,13 @@ import personalizedreciperecommenderlogo from '../../assets/personalizedrecipere
 import db2apilogo from '../../assets/db2apilogo.webp'
 import verdictailogo from '../../assets/verdictailogo.webp'
 import customerchurnpredictionlogo from '../../assets/customerchurnpredictionlogo.webp'
-
+import coldemailautomationlogo from '../../assets/ColdEmailGeneratorlogo.webp'
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
         <h1 className="sectionTitle">Academic Projects</h1>
         <div className={styles.projectContainer}>
+        <ProjectCard src={coldemailautomationlogo} link={"https://github.com/RajAdroja/Cold-Email-Generator"} h3={"LLM Project"} p={"Cold Email Generator"}/>
           <ProjectCard src={viberr} link={"https://github.com/RajAdroja/Automatic-colorization-using-auto-encoder"} h3={"DL Project"} p={"Automatic Colorization"}/>
           <ProjectCard src={projectphoto} link={"https://github.com/RajAdroja/Sentiment-Analysis"} h3={"ML Project"} p={"Sentiment Analysis"}/>
           <ProjectCard src={fitLift} link={"https://www.vpropel.in/"} h3={"V-PROPEL portal"} p={"Coding Platform"}/>
@@ -26,8 +27,8 @@ function Projects() {
           <ProjectCard src={restaurantfinderlogo} link={"https://github.com/RajAdroja/RestaurantFinder"} h3={"RestaurantFinder"} p={"End2End WebApp"}/>
           <ProjectCard src={logparserlogo} link={"https://github.com/RajAdroja/LogParser"} h3={"LogParser"} p={"Java Project"}/>
           <ProjectCard src={personalizedreciperecommenderlogo} link={"https://github.com/RajAdroja/Personalized-Recipe-Recommender"} h3={"Recipe Recommender"} p={"ML Project"}/>
-          <ProjectCard src={db2apilogo} link={"https://github.com/RajAdroja/db2api"} h3={"db2appi"} p={"Schema Translation"}/>
-          <ProjectCard src={verdictailogo} link={"https://github.com/RajAdroja/VerdictAI"} h3={"VerdictAI"} p={"LLM Project"}/>
+          <ProjectCard src={db2apilogo} link={"https://github.com/RajAdroja/db2api"} h3={"db2api"} p={"Schema Translation"}/>
+          {/* <ProjectCard src={verdictailogo} link={"https://github.com/RajAdroja/VerdictAI"} h3={"VerdictAI"} p={"LLM Project"}/> */}
           <ProjectCard src={customerchurnpredictionlogo} link={"https://github.com/RajAdroja/Customer-Churn-Prediction"} h3={"Customer Churn"} p={"End to End ML pipeline"}/>
         </div>
         <br />
